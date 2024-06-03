e472a059a6c5d13ca7221e64f0ac78bf4693b40f
yan234rrff <iarovenkoigor@yandex.ru>
2024-06-03 21:17:01 +0300

    Обнова

--- Assets/PlayerConroller.cs
+++ Assets/PlayerConroller.cs
@@ -13,6 +13,8 @@ public class PlayerConroller : MonoBehaviour
     private Transform cam;
     private Animator anim;
     [SerializeField] TextMeshProUGUI text;
+    private bool run = false;
+    private bool jump = false;
 
     public void Move(InputAction.CallbackContext context)
     {
@@ -24,18 +26,136 @@ public class PlayerConroller : MonoBehaviour
         pointer = context.ReadValue<Vector2>();
     }
 
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hit, 2, ~(1 << 3)))
+        {
+            if (hit.collider.gameObject.TryGetComponent<Interactable>(out Interactable i))
+            {
+                i.Interact();
+            }
+        }
+    }
+
+    public void Run(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            run = true;
+            return;
+        }
+        if (context.canceled)
+        {
+            run = false;
+        }
+        
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            jump = true;
+            return;
+        }
+        if (context.canceled)
+        {
+            jump = false;
+        }
+
+    }
+
+
     public void Update()
     {
         Vector3 moveDirection = speed * (transform.right * direction.x + transform.forward * direction.y).normalized;
-        anim.SetBool("Move", (direction.x > 0 || direction.y > 0));
+        if (direction.x > 0)
+        {
+            if (direction.y > 0)
+            {
+                anim.SetBool("Forward",true);
+                if (run)
+                {
+                    anim.SetBool("Run", true);
+                }
+                else
+                {
+                    anim.SetBool("Run",false);
+                }
+            }
+            else if (direction.y < 0)
+            {
+                anim.SetBool("Right",false);
+                anim.SetBool("Back",true);
+            }
+            else
+            {
+                anim.SetBool("Right",true); 
+            }
+        }
+        else if (direction.x < 0)
+        {
+            if (direction.y > 0)
+            {
+                anim.SetBool("Forward", true);
+                if (run)
+                {
+                    anim.SetBool("Run", true);
+                }
+                else
+                {
+                    anim.SetBool("Run", false);
+                }
+            }
+            else if (direction.y < 0)
+            {
+                anim.SetBool("Left", false);
+                anim.SetBool("Back", true);
+            }
+            else
+            {
+                anim.SetBool("Left", true);
+            }
+        }
+        else
+        {
+            if (direction.y > 0)
+            {
+                anim.SetBool("Forward", true);
+                if (run)
+                {
+                    anim.SetBool("Run", true);
+                }
+                else
+                {
+                    anim.SetBool("Run", false);
+                }
+            }
+            else if (direction.y < 0)
+            {
+                anim.SetBool("Back", true);
+            }
+            else
+            {
+                anim.SetBool("Right", false);
+                anim.SetBool("Left", false);
+                anim.SetBool("Back",false);
+                anim.SetBool("Forward",false);
+                anim.SetBool("Run", false);
+            }
+        }
         float y = rb.velocity.y;
+        if (run && moveDirection.y > 0)
+        {
+            moveDirection *= 1.5F;
+        }
         moveDirection.y = y;
         rb.velocity = moveDirection;
         transform.eulerAngles += new Vector3(0, pointer.x * sensitivityX, 0);
         cameraRot.x += -pointer.y * sensitivityY;
         cameraRot.x = Mathf.Clamp(cameraRot.x, -90, 90);
         cam.localRotation = Quaternion.Euler(cameraRot);
-        if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hit, 5, ~(1<<3)))
+        if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hit, 2, ~(1<<3)))
         {
             if (hit.collider.gameObject.TryGetComponent<Interactable>(out Interactable i))
             {
